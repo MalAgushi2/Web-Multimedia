4f11eb825de9eb3d765766270421b8f024e10ebf
Mal Agushi <maliagushi6@gmail.com>
2024-06-10 21:00:08 +0200

    Initial commit of ebook-store project

---
+++ src/components/EbookCover.js
@@ -0,0 +1,14 @@
+// src/components/EbookCover.js
+import React from 'react';
+import './EbookCover.css';
+
+const EbookCover = ({ coverImage, title, onClick }) => {
+  return (
+    <div className="ebook-cover" onClick={onClick}>
+      <img src={coverImage} alt={`${title} cover`} />
+      <h3>{title}</h3>
+    </div>
+  );
+};
+
+export default EbookCover;
